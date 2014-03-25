e1c9a7494332a8b2fc42a1a37e0be927eef97a45
bhalash <mark@bhalash.com>
2014-03-25 15:29:50 +0000

    Flexible gallery now works.

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,52 +1,56 @@
-// Pre-shrink row height.
-var rowHeight = 700;
-// Parent container width.
-var galleryWidth = $('.gallery').width();
+function imageDebugInfo() {
+    // Logs debug info on each row and image therein.
+    var img = sum = row = 0;
 
-function galleryImgSizes() {
+    $('.row').each(function() {
+        console.log('Row #' + row + ':\n----');
+
+        $(this).children('img').each(function() {
+            console.log(
+                'img #' + img + ': '  
+                + 'w: ' + $(this).width()
+                + ' h: '+ $(this).height()
+            );
+
+            img++;
+        });
+
+        sum = sumImgRowWidth(this);
+        console.log('Total:\tw: ' + sum + '\n');
+        row++;
+        img = 0;
+    });
+}
+
+function resizeImages() {
+    // Resizes each row (more-or-less) equally.
+    // I have run up against rounding problems, so the image sizes are fudged down by 1px.
+    // Two nested loops: Rows, and then images.
+    // 
+    // For each row:
+    // 1. Resize each image to the same height.
+    // 2. Determine length of the row of images.
+    // 3. Determine the ratio between the row and the gallery width.
+    // 4. Resize each image down by this ratio.
     $('.row').each(function() {
         var sum = ratio = 0;
-        // Set each image to a universal size.
-        // Images will be resized again later.
-        setImgSize(this, rowHeight); 
-        // Find the total length of the row with each image of the same height.
-        sum = sumImgRowWidth(this);
-        ratio = galleryWidth / sum;
-        console.log(ratio);
-        // Fudge until I lock down row length.
-        setImgSize(this, ratio);
-    });
-}
 
-function sumImgRowWidth(obj) {
-    var a = 0;
+        $(this).children('img').each(function() {
+            sum += $(this).width();
+        });
 
-    $(obj).children('img').each(function() {
-        // Set heights universally. This will change later. 
-        // Record widths.
-        a += $(this).width();
-    });
+        var ratio = parseFloat($('.gallery').width() / sum);
 
-    console.log(a);
-    return a;
-}
-
-function setImgSize(obj, size) {
-    // Resize each image height down according to the ratio.
-    // Width is set to 'auto' in the CSS.
-    $(obj).children('img').each(function() {
-        if (size < 10) {
-            $(this).css('height', $(this).height() * size + 'px');
-        } else {
-            $(this).css('height', size + 'px');
-        }
+        $(this).children('img').each(function() {
+            $(this).css('height', Math.floor($(this).height() * ratio) + 'px');
+        });
     });
 }
 
 $(function() {
-    galleryImgSizes();
+    resizeImages();
 });
 
 $(window).resize(function() {
-    galleryImgSizes();
+    resizeImages();
 });
