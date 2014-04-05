a7e7924c7b78271231b94b0d4be9b46b82a44bc7
bhalash <mark@bhalash.com>
2014-04-05 06:31:09 +0100

    Gallery complete. Resumed work on lightbox.

--- uber-gallery/gallery.js
+++ uber-gallery/gallery.js
@@ -1,21 +1,42 @@
-// Gallery div name.
-var gal = 'uber-gallery';
-// Gallery row name.
-var row = 'uber-row';
-// Minimum and maximum row size.
-var rowMin = 4;
-var rowMax = 6;
+// Gallery, row, and img classes.
+// Update gallery.css if you change these!!!!
+var galName     = 'uber-gallery';
+var rowName     = galName + '-row';
+var imgName     = galName + '-img';
+var boxName     = galName + '-box';
+// Maximum, minimum, and tiny row sizes. Tiny is used in mobile views.
+var rowSizeMin  = 4;
+var rowSizeMax  = 6;
+var rowSizeTiny = 3;
+// Break point before switching to mobile view.
+var mobileSize  = 880;
+// Include image alt text as anchor title?
+var altAsTitle  = false;
+// Debug. Replace anchor hyperlinks with "javascript:void(0)".
+var voidHref    = true;
 
-$('.' + row + ' img').click(function() {
-    alert('yolo');
-});
+/*
+    GALLERY
+*/
+
+function addAnchor(obj, addTitle) {
+    // Turn static image into a clickable hyperlink. 
+    $(obj).children('img').each(function() {
+        if (!$(this).parent().is('a')) {
+            // Hyperlink and title for anchor.
+            var href  = (voidHref === true) ? 'href="javascript:void(0)" ' : 'href="' + $(this).attr('src') + '" ';
+            var title = (addTitle === true) ? ' title="' + $(this).attr('alt') + '" ' : ' ';
+            $(this).wrap('<a ' + href  + title + ' ></a>');
+        }
+    });
+}
 
 function addGalleryID(obj) {
     // Give each gallery a unique ID.
     var count = 0;
 
     $(obj).each(function () {
-        $(this).attr('id', 'ug-' + count);
+        $(this).attr('id', rowName + ' ' + count);
         count++;
     }); 
 }
@@ -23,7 +44,7 @@ function addGalleryID(obj) {
 function addRow(obj) {
     // Append a row to gallery.
     // <div class="uber-row"></div>
-    $(obj).append('<div class="' + row + '"></div>');
+    $(obj).append('<div class="' + rowName + '"></div>');
 }
 
 function sRandom(min,max) {
@@ -34,18 +55,21 @@ function sRandom(min,max) {
 function addGalleryRows(obj) {
     // Create an array of images, loop through, adding images to rows.
     // 1. Add a row.
-    // 2. Insert images until it has between rowMin and rowMax.
+    // 2. Insert images until it has between rowSizeMin and rowSizeMax.
     // 3. Insert new row and repeat.
     // 4. Don't insert a new row if only 1 or 2 images are left.
     var imgArr = $(obj).children('img').toArray();
     addRow(obj);
 
     $(imgArr).each(function (i) {
-        $('.' + row).last().append(imgArr[i]); 
         // Smaller row size on smaller screens.
-        var rowLength = ($(window).width() > 800) ? sRandom(rowMin,rowMax) : 3;
+        var rowLength = ($(window).width() > mobileSize) ? sRandom(rowSizeMin,rowSizeMax) : rowSizeTiny;
+        $('.' + rowName).last().append(imgArr[i]); 
 
-        if ($('.' + row).last().children().size() >= rowLength && (imgArr.length - 1 - i) >= 2) {
+        // Add a new row if the length exceeds our quasi-random size.
+        // Do not add a new row if we are at the end of the array and only 1 or 2 images remain.
+        // The effect of a single-image row is ugly and a thing to be avoided.
+        if ($('.' + rowName).last().children().size() >= rowLength && (imgArr.length - 1 - i) >= 2) {
             addRow(obj);
         }
     });
@@ -81,7 +105,7 @@ function getRowWidth(obj) {
 
 function orderRowImages(obj) {
     // Resizes each row of images such as to evenly space their width and height. 
-    $(obj).children('.' + row).each(function () {
+    $(obj).children('.' + rowName).each(function () {
         // Get total width of row through width of component images.
         var sum = getRowWidth(this);
         // Ratio between gallery width, and row width.
@@ -89,9 +113,10 @@ function orderRowImages(obj) {
 
         $(this).children('img').each(function () {
             // Change the height of the image by the ratio.
+            // Add appropriate class for click events.
             var newHeight = Math.round($(this).height() * ratio);
             changeObjHeight(this, newHeight);
-            // $(this).wrap('<a class="foo" href="#"></a>');
+            $(this).attr('class', imgName);
         });
 
         // Rounding errors leave a small margin on the right side of the gallery.
@@ -100,18 +125,60 @@ function orderRowImages(obj) {
         var diff = getRowWidth(this) - ($(obj).width() - 1);
         // Resize the last image in line to make it all fit.
         // A smaller row is made slightly larger and vice versa.
-        $(this).children('img:last-child').css(
-            'width',
-            $(this).children('img:last-child').width() - diff + 'px'
-        );
+        $(this).children('img').last().css('width', $(this).children('img').last().width() - diff + 'px');
+
+        // Add anchor element.
+        addAnchor(this, altAsTitle);
     });
 }
 
+/*
+    LIGHTBOX
+*/
+
+function addLightbox(obj) {
+    // Append empty lightbox to whatever.
+    // Best prepended to body.
+    $(obj).prepend(
+        '<div class="' + boxName + '"><div class="' + boxName + '-img"></div></div>');
+    changeObjHeight('.' + boxName, $(window).height());
+}
+
+function toggleLightbox() {
+    // Toggle appearance of the lightbox.
+    var lb = $('.' + boxName);
+    var lbDisp = (lb.css('display') === 'none') ? 'initial' : 'none';
+    $(lb).css('display', lbDisp);
+}
+
+function setLightboxImg(obj) {
+    // Add clicked image to lightbox.
+    var lb = $('.' + boxName + '-img');
+    lb.empty();
+    lb.append('<img class="snusnu" src="' + $(obj).attr('src') + '" />');
+    // Vertically center photo.
+    changeObjHeight('.snusnu', $(window).height() * 0.9);
+    vertCenterObj('.snusnu');
+}
+
 $(window).load(function() {
-    $('.' + gal).each(function() { 
+    $('.' + galName).each(function() { 
         addGalleryID(this);
         addGalleryRows(this);
         orderRowImages(this);
+    });
+
+    addLightbox('body');
+
+    $('.' + imgName).click(function() {
+        // Show box.
+        toggleLightbox();
+        setLightboxImg(this);
+    });
+
+    $('.' + boxName).click(function() {
+        // Hide box (debug).
+        toggleLightbox();
     });
 });
 
